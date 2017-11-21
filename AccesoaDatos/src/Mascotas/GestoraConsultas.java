e63f729fc8a6fa82b340e5dd889de0ccd689778d
Unknown <07.qweasd123@gmail.com>
2017-11-21 09:33:28 +0100

    up

--- AccesoaDatos/src/Mascotas/GestoraConsultas.java
+++ AccesoaDatos/src/Mascotas/GestoraConsultas.java
@@ -5,6 +5,12 @@
  */
 package Mascotas;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author icastillo
@@ -20,7 +26,47 @@ public class GestoraConsultas {
         return conexion;
     }
     
+    /*
+    Proposito: Carga en un ResultSet en contenido de la tabla BI_Actualizaciones
+    Precondiciones: La tabla BI_Actualizaciones debe contener algún dato
+    Entradas: No hay
+    Salidas: Un ResultSet con el resultado de la consulta a BI_Actualizaciones
+    Postcondiciones: Se ha cargado en un ResultSet el contenido de la tabla BI_Actualizaciones
+    */
+    public ResultSet cargaContenidoActualizaciones(){
+       // conexion.connect();
+        String consulta="SELECT * FROM BI_Actualizaciones";
+        Statement sentencia;
+        ResultSet resultSet=null;
+        
+        try {
+            sentencia=conexion.getConnect().createStatement();
+            resultSet=sentencia.executeQuery(consulta);                                          
+        } catch (SQLException ex) {
+            Logger.getLogger(GestoraConsultas.class.getName()).log(Level.SEVERE, null, ex);
+        }      
+        return resultSet;
+    }
     
+     /*
+    Proposito: Según el estado de las columnas raza, especie y enfermedad, 
+               decidirá si realiza un a actualización o una inserción en las distintas tablas 
+    Precondiciones: La tabla BI_Actualizaciones debe contener algún dato
+    Entradas: No hay
+    Salidas: Un ResultSet con el resultado de la consulta a BI_Actualizaciones
+    Postcondiciones: Se ha cargado en un ResultSet el contenido de la tabla BI_Actualizaciones
+    */
+    public void routerXD(ResultSet resultSet){
+        if(resultSet!=null){
+            try {
+                while(resultSet.next()){
+                    
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(GestoraConsultas.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
     
     
 }
