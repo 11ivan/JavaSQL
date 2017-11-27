49469ab929e8c19c1674a7f6f757099626f0fe74
Unknown <07.qweasd123@gmail.com>
2017-11-27 10:19:41 +0100

    up

--- AccesoaDatos/src/Mascotas/GestoraConsultas.java
+++ AccesoaDatos/src/Mascotas/GestoraConsultas.java
@@ -28,7 +28,7 @@ public class GestoraConsultas {
     
     /*
     Proposito: Carga en un ResultSet en contenido de la tabla BI_Actualizaciones
-    Precondiciones: La tabla BI_Actualizaciones debe contener algún dato
+    Precondiciones: No hay
     Entradas: No hay
     Salidas: Un ResultSet con el resultado de la consulta a BI_Actualizaciones
     Postcondiciones: Se ha cargado en un ResultSet el contenido de la tabla BI_Actualizaciones
@@ -57,9 +57,39 @@ public class GestoraConsultas {
     Postcondiciones: Se ha cargado en un ResultSet el contenido de la tabla BI_Actualizaciones
     */
     public void routerXD(ResultSet resultSet){
+        //String fecha
+        String temperatura;
+        String peso;
+        String codigoMascota;
+        String raza;
+        String especie;
+        String fechaNac;
+        String fechaFall;
+        String alias;
+        String codigoPropietario;
+        String enfermedad;
         if(resultSet!=null){
             try {
                 while(resultSet.next()){
+                    //insercion en la table BI_Mascotas mediante resulset actualizable
+                    
+                    //inserciones en BI_MascotasEnfermedades deben hacerse con una sentencia preparada (Prepared Statement o CallableStatement).
+                    //Esta sentencia debe ejecutar el procedimiento almacenado del ejercicio 1.
+                    
+                    //Las inserciones en BI_Visitas las puedes hacer como mejor te parezca.
+
+                    //Si la raza, la especie y enfermedad son null a enfermedad es que la mascota ya está registrada,
+                    //será inserción sólo en BI_Visitas
+                    
+                    //Si la raza, epecie son null pero la enfermedad no es que la mascota ya está registrada,
+                    //se hará la inserción en BI_MascotasEnfermedades y BI_Visitas
+                    
+                    //Si la raza y especie no son null pero la enfermedad sí, es que la mascota no está registrada, 
+                    //se hará una inserción en la tabla BI_Mascotas, BI_Visitas y BI_Clientes
+                    
+                    //Si la raza, especie y enfermedad no son null, es que la mascota no está registrada, 
+                    //se hará una inserción en la tabla BI_Mascotas, BI_Visitas, BI_Clientes y BI_MascotasEnfermedades
+                    
                     
                 }
             } catch (SQLException ex) {
@@ -69,13 +99,20 @@ public class GestoraConsultas {
     }
     
     /*
-    Proposito: Comprueba si es necesario realizar una actualizacion o una inserción 
-    Precondiciones: La tabla BI_Actualizaciones debe contener algún dato
+    Proposito: Realiza una insercion en la table BI_Mascotas mediante resulset actualizable
+    Precondiciones: No hay
     Entradas: No hay
-    Salidas: Un ResultSet con el resultado de la consulta a BI_Actualizaciones
-    Postcondiciones: Se ha cargado en un ResultSet el contenido de la tabla BI_Actualizaciones
+    Salidas: Un entero con la cantidad de filas afectadas
+    Postcondiciones: Se ha insertado en la Tabla BI_Mascotas los nuevos datos
     */
-
+    public int insertInTOMascotas(){
+        int filasAfectadas=0;
+    
+        
+        
+        
+        return filasAfectadas;
+    }
     
     
 }
