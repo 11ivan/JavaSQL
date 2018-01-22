ec3b9ca621eb74d346901b301fc480ff52ba8eea
Unknown <07.qweasd123@gmail.com>
2018-01-22 10:15:06 +0100

    up

---
+++ HibernateExample/src/hibernateexample/HibernateExample.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hibernateexample;
+
+import java.util.Iterator;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author icastillo
+ */
+public class HibernateExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+     SessionFactory instancia = HibernateUtil.buildSessionFactory();
+        Session session = instancia.openSession();
+        Query q = session.createQuery("from Empleados");
+ 
+        Iterator<Empleados> it = q.iterate();
+ 
+        Empleados e;
+        while (it.hasNext()) {
+            e = it.next();
+            System.out.println(e.getNomEmp() + " " + e.getSalEmp());
+        }
+ 
+        session.close();
+    }   
+        
+    }
+}
